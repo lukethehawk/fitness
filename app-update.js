8880b8f1fc227b3814516eca15335a715d2b4788
lukethehawk <98876950+lukethehawk@users.noreply.github.com>
2026-06-19 21:37:58 +0200

    Bump version for corrected spacing

--- app-update.js
+++ app-update.js
@@ -1,6 +1,6 @@
 "use strict";
 (function installAppUpdates(){
-  const APP_VERSION="1.0.44";
+  const APP_VERSION="1.0.45";
   let pendingWorker=null;
   let refreshing=false;
   let updateNoticeShown=false;
