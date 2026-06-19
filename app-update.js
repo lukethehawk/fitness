eb2f7c2bea6782ddd62152f25df6328a72c81c31
lukethehawk <98876950+lukethehawk@users.noreply.github.com>
2026-06-19 21:06:23 +0200

    Bump production version

--- app-update.js
+++ app-update.js
@@ -1,6 +1,6 @@
 "use strict";
 (function installAppUpdates(){
-  const APP_VERSION="1.0.40";
+  const APP_VERSION="1.0.41";
   let pendingWorker=null;
   let refreshing=false;
   let updateNoticeShown=false;
